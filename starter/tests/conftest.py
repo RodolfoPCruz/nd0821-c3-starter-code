3995ea2089591560b06148344ffcae67cb7bbafb
Rodolfo <rodolfo_pinheiro@hotmail.com>
2025-02-19 09:16:30 -0300

    Added test for three function in the basic_cleaning.py file. The functions are removing_whitespaces, removing duplicates and saving dataframe

--- starter/tests/conftest.py
+++ starter/tests/conftest.py
@@ -4,11 +4,10 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
-from constants import raw_file_path
-
+from constants import raw_file_path, cleaned_file_path
+from basic_cleaning import import_data
 
 @pytest.fixture
-
 def param_raw_file_path():
     """
     Provides the path to load the raw dataframe
@@ -18,3 +17,35 @@ def param_raw_file_path():
     """
 
     return raw_file_path
+
+@pytest.fixture
+def param_raw_data():
+    """
+    Provides dataframe with the raw data
+
+    Return 
+        pandas dataframe: initial dataset
+    """
+    raw_dataframe = import_data(raw_file_path)
+    return raw_dataframe
+
+@pytest.fixture
+def param_cleaned_data_path():
+    """
+    Provides the path of the cleaned dataframe
+
+    Return 
+        str: path of the clened dataset file
+    """
+    return cleaned_file_path
+
+@pytest.fixture
+def param_clened_data():
+    """
+    Provides the cleaned dataframe
+
+    Return 
+        pandas dataframe: cleaned dataset
+    """
+    cleaned_dataframe = import_data(cleaned_file_path)
+    return cleaned_dataframe
--- starter/tests/test_basic_cleaning.py
+++ starter/tests/test_basic_cleaning.py
@@ -8,8 +8,11 @@ import logging
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
 from basic_cleaning import import_data
+from basic_cleaning import removing_whitespaces
+from basic_cleaning import removing_duplicates
+from basic_cleaning import saving_dataframe
 from constants import log_file_test_path
-
+from constants import cleaned_file_path
 
 logging.basicConfig(
     filename = log_file_test_path,
@@ -38,4 +41,59 @@ def test_import(param_raw_file_path):
         assert df.shape[1] > 0
     except AssertionError as err:
         logging.error("The dataframe is empty")
+        raise err
+    
+def test_removing_whitespaces(param_raw_data):
+    """
+    Test removing_whitespace function
+
+    Args:
+        raw_data(pd.DataFrame): initial dataset
+    """
+    logging.info("Testing removing_whitespaces function")
+    try:
+        df = removing_whitespaces(param_raw_data)
+        column_names = df.columns.to_list()
+        leading_or_trailing_spaces = [name.startswith(" ") or name.endswith(" ") for name in column_names]
+        assert not any(leading_or_trailing_spaces)
+        logging.info('Sucess: There are not leading or trailing spaces in the column names')
+    except AssertionError as err:
+        logging.error("There are leading spaces or trailing spaces in the column names")
+        raise err
+
+def test_removing_duplicates(param_raw_data):
+    """
+    Test removing_duplicates function
+
+    Args:
+        raw_data(pd.DataFrame): initial dataset
+    """
+    logging.info("Testing removing_duplicates function")
+    try:
+        df = removing_duplicates(param_raw_data)
+        assert not df.duplicated().any()
+        logging.info('Sucess: There are not duplicated rows')
+    except AssertionError as err:
+        logging.error('There are duplicated rows')
+        raise err
+
+def test_saving_dataframe(param_raw_data, param_cleaned_data_path):
+    '''
+    Test saving_dataframe function
+
+    Args:
+        raw_data(pd.DataFrame): initial dataset
+        cleaned
+
+    '''
+    logging.info('Testing saving_dataframe function')
+    df = removing_whitespaces(param_raw_data)
+    df = removing_duplicates(df)
+    saving_dataframe(df, param_cleaned_data_path)
+
+    try:
+        assert os.path.exists(param_cleaned_data_path)
+        logging.info('Sucess: The file was saved')
+    except AssertionError as err:
+        logging.error('The file was not saved')
         raise err
